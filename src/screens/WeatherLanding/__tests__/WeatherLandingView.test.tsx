49ec5655da51e7c4d90841a4a111401e57ad7807
Dzmitry Hrantsevich <Dzmitry.Hrantsevich@ventionteams.com>
2024-07-22 00:48:10 +0200

    updated tests

--- src/screens/WeatherLanding/__tests__/WeatherLandingView.test.tsx
+++ src/screens/WeatherLanding/__tests__/WeatherLandingView.test.tsx
@@ -2,81 +2,61 @@ import { render, screen } from '@testing-library/react-native';
 import { WeatherLandingView } from '../WeatherLandingView';
 
 describe('WeatherLandingView', () => {
-  it.each([
-    [false, true, true],
-    [true, false, false],
-    [true, false, true],
-    [false, true, false],
-  ] as const)(
-    'sectons render correctly with isSearchSectionEnabled=%s isGeolocationSectionEnabled=%s isFavouriteSectionEnabled=%s',
-    (isSearchSectionEnabled, isGeolocationSectionEnabled, isFavouriteSectionEnabled) => {
-      render(
-        <WeatherLandingView
-          isFavouriteLocationWeatherLoading
-          isFavouriteSectionEnabled={isFavouriteSectionEnabled}
-          isGeolocationSectionEnabled={isGeolocationSectionEnabled}
-          isGeolocationWeatherLoading
-          isSearchLocationWeatherLoading
-          isSearchSectionEnabled={isSearchSectionEnabled}
-          onCardPress={jest.fn()}
-        />,
-      );
+  it('sections render correctly', () => {
+    render(
+      <WeatherLandingView
+        isFavouriteLocationWeatherLoading
+        isFavouriteSectionEnabled={false}
+        isGeolocationSectionEnabled
+        isGeolocationWeatherLoading
+        isSearchLocationWeatherLoading
+        isSearchSectionEnabled={false}
+        onCardPress={jest.fn()}
+      />,
+    );
 
-      if (isSearchSectionEnabled) expect(screen.queryByText('Search Location')).toBeOnTheScreen();
-      else expect(screen.queryByText('Search Location')).not.toBeOnTheScreen();
+    expect(screen.queryByText('Search Location')).not.toBeOnTheScreen();
+    expect(screen.getByText('My Location')).toBeOnTheScreen();
+    expect(screen.queryByText('Favourite Location')).not.toBeOnTheScreen();
+  });
 
-      if (isGeolocationSectionEnabled) expect(screen.queryByText('My Location')).toBeOnTheScreen();
-      else expect(screen.queryByText('My Location')).not.toBeOnTheScreen();
+  it('section errors display correctly', () => {
+    render(
+      <WeatherLandingView
+        favouriteLocationWeatherError={'Fav Error'}
+        geolocationWeatherError={'Geo Error'}
+        isFavouriteLocationWeatherLoading
+        isFavouriteSectionEnabled
+        isGeolocationSectionEnabled
+        isGeolocationWeatherLoading
+        isSearchLocationWeatherLoading
+        isSearchSectionEnabled
+        onCardPress={jest.fn()}
+        searchLocationWeatherError={'Search Error'}
+      />,
+    );
 
-      if (isFavouriteSectionEnabled)
-        expect(screen.queryByText('Favourite Location')).toBeOnTheScreen();
-      else expect(screen.queryByText('Favourite Location')).not.toBeOnTheScreen();
-    },
-  );
+    expect(screen.getByText('Search Error')).toBeOnTheScreen();
+    expect(screen.getByText('Geo Error')).toBeOnTheScreen();
+    expect(screen.getByText('Fav Error')).toBeOnTheScreen();
 
-  it.each([
-    ['search error', undefined, undefined, true],
-    [undefined, 'geolocation error', undefined, true],
-    [undefined, undefined, 'favourite error', true],
-    ['search error', 'geolocation error', 'favourite error', false],
-  ] as const)(
-    'errors display correctly with searchLocationWeatherError=%s geolocationWeatherError=%s favouriteLocationWeatherError=%s isEnabled',
-    (
-      searchLocationWeatherError,
-      geolocationWeatherError,
-      favouriteLocationWeatherError,
-      isEnabled,
-    ) => {
-      render(
-        <WeatherLandingView
-          favouriteLocationWeatherError={favouriteLocationWeatherError}
-          geolocationWeatherError={geolocationWeatherError}
-          isFavouriteLocationWeatherLoading
-          isFavouriteSectionEnabled={isEnabled}
-          isGeolocationSectionEnabled={isEnabled}
-          isGeolocationWeatherLoading
-          isSearchLocationWeatherLoading
-          isSearchSectionEnabled={isEnabled}
-          onCardPress={jest.fn()}
-          searchLocationWeatherError={searchLocationWeatherError}
-        />,
-      );
+    render(
+      <WeatherLandingView
+        favouriteLocationWeatherError={'Fav Error'}
+        geolocationWeatherError={'Geo Error'}
+        isFavouriteLocationWeatherLoading
+        isFavouriteSectionEnabled={false}
+        isGeolocationSectionEnabled
+        isGeolocationWeatherLoading
+        isSearchLocationWeatherLoading
+        isSearchSectionEnabled={false}
+        onCardPress={jest.fn()}
+        searchLocationWeatherError={'Search Error'}
+      />,
+    );
 
-      if (isEnabled) {
-        if (searchLocationWeatherError)
-          expect(screen.queryByText(searchLocationWeatherError)).toBeOnTheScreen();
-        if (geolocationWeatherError)
-          expect(screen.queryByText(geolocationWeatherError)).toBeOnTheScreen();
-        if (favouriteLocationWeatherError)
-          expect(screen.queryByText(favouriteLocationWeatherError)).toBeOnTheScreen();
-      } else {
-        if (searchLocationWeatherError)
-          expect(screen.queryByText(searchLocationWeatherError)).not.toBeOnTheScreen();
-        if (geolocationWeatherError)
-          expect(screen.queryByText(geolocationWeatherError)).not.toBeOnTheScreen();
-        if (favouriteLocationWeatherError)
-          expect(screen.queryByText(favouriteLocationWeatherError)).not.toBeOnTheScreen();
-      }
-    },
-  );
+    expect(screen.queryByText('Search Error')).not.toBeOnTheScreen();
+    expect(screen.getByText('Geo Error')).toBeOnTheScreen();
+    expect(screen.queryByText('Fav Error')).not.toBeOnTheScreen();
+  });
 });
