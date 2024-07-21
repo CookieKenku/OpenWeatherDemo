f137222c3e6257cef49de67b11cce2751aa5c529
Dzmitry Hrantsevich <Dzmitry.Hrantsevich@ventionteams.com>
2024-07-21 20:49:06 +0200

    added tests

---
+++ src/helpers/__tests__/formatTempValue.test.tsx
@@ -0,0 +1,20 @@
+import { formatTempValue } from '../formatTempValue';
+
+describe('formatTempValue', () => {
+  it.each([
+    [10.69, '11°'],
+    [10.1, '10°'],
+    [-1, '-1°'],
+    [0, '0°'],
+  ] as const)('returns formatted value with value=%s', (value, expectedValue) => {
+    const result = formatTempValue(value);
+
+    expect(result).toBe(expectedValue);
+  });
+
+  it('handles falsy value', () => {
+    const result = formatTempValue();
+
+    expect(result).toBe('-°');
+  });
+});
